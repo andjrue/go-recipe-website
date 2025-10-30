fefafbfc8e80e2dc7c4ad2ab6370004b97ce63f6
andjrue <drewsimcox12@gmail.com>
2025-10-29 23:08:03 -0600

    migrations checkpoint

--- internal/database/migrations/20251030043355_users.sql
+++ internal/database/migrations/20251030043355_users.sql
@@ -1,9 +1,18 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS users (
+    user_id TEXT PRIMARY KEY DEFAULT gen_random_uuid()::text,
+    hashed_password TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    alias VARCHAR(25) NOT NULL UNIQUE CHECK (length(alias) > 3),
+    date_joined TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS idx_user_email ON users(email);
+CREATE INDEX IF NOT EXISTS idx_user_alias ON users(alias);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE IF EXISTS users;
 -- +goose StatementEnd
