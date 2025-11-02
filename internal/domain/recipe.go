8cf4211c0dd1757348bf000310b2d4505ed90ded
andjrue <drewsimcox12@gmail.com>
2025-11-02 02:59:27 -0700

    chore: add json tags, db next

--- internal/domain/recipe.go
+++ internal/domain/recipe.go
@@ -7,27 +7,28 @@ import (
 	"github.com/google/uuid"
 )
 
+// TODO: Add db tags to domain objs
 type Recipe struct {
-	RecipeID     uuid.UUID
-	UserID       uuid.UUID
-	Name         string
-	TimeToCook   *int
-	Description  *string
-	DatePosted   time.Time
-	Deleted      bool
-	LastEditedAt time.Time
+	RecipeID     uuid.UUID `json:"recipe_id"`
+	UserID       uuid.UUID `json:"user_id"`
+	Name         string `json:"name"`
+	TimeToCook   *int `json:"time_to_cook"`
+	Description  *string `json:"decription"`
+	DatePosted   time.Time `json:"date_posted"`
+	Deleted      bool `json:"deleted"`
+	LastEditedAt time.Time `json:"last_edited_at"`
 }
 
 type CreateRecipeRequest struct {
-	Name        string
-	TimeToCook  *int
-	Description *string
-	UserID      uuid.UUID
+	Name        string `json:"name"`
+	TimeToCook  *int `json:"time_to_cook"`
+	Description *string `json:"decription"`
+	UserID      uuid.UUID `json:"user_id"`
 }
 
 type UpdateRecipeRequest struct {
-	Name        *string
-	TimeToCook  *int
-	Description *string
-	RecipeID    uuid.UUID
+	Name        string `json:"name"`
+	TimeToCook  *int `json:"time_to_cook"`
+	Description *string `json:"decription"`
+	RecipeID    uuid.UUID `json:"recipe_id"`
 }
