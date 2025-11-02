ec6ac8f04c692410ad23ad6ceedda2b53b4c72e4
andjrue <drewsimcox12@gmail.com>
2025-11-02 02:27:32 -0700

    Small updates, add constructor for centralized repository creation

--- internal/repository/recipe_repository.go
+++ internal/repository/recipe_repository.go
@@ -6,15 +6,22 @@ import (
 	"recipe-website/internal/domain"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
-type RecipeInt interface { // Not appropriate naming, but it will probably
+type RecipeRepository interface {
 	GetByPK(ctx context.Context, recipeID uuid.UUID) (*domain.Recipe, error)
 	GetAllByUserID(ctx context.Context, userID uuid.UUID) ([]domain.Recipe, error)
 	Create(ctx context.Context, recipe *domain.CreateRecipeRequest) (*domain.Recipe, error)
-	Update(ctx context.Context, req *domain.UpdateRecipeRequest) (*domain.Recipe, error)
+	Update(ctx context.Context, req *domain.UpdateRecipeRequest) error
 	Delete(ctx context.Context, recipeID uuid.UUID) error
 	// recover -- we only soft delete, so recipes will be recoverable
 	// get all deleted -- need a menu to see all deleted recipes by a user
 	// perm delete -- remove recipe from db
+}
+
+func NewRecipeRepository(db *pgx.Conn) RecipeRepository {
+	return &PostgresRecipeRepository{
+		db: db,
+	}
 }
