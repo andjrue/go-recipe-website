09ce50ae71a79927ad3f0c5c08a1a206459b67c7
andjrue <drewsimcox12@gmail.com>
2025-11-01 19:46:26 -0600

    fmt updates

--- internal/service/recipe_service.go
+++ internal/service/recipe_service.go
@@ -83,12 +83,12 @@ func (s *RecipeService) GetAllByUserID(ctx context.Context, userID uuid.UUID) ([
 	if err := uuid.Validate(userID.String()); err != nil {
 		return nil, fmt.Errorf("recipe id is required, GetByID")
 	}
-	
+
 	recipes, err := s.GetAllByUserID(ctx, userID)
 	if err != nil {
 		return nil, fmt.Errorf("[recipe service] - unable to fetch all for user: %v", err)
 	}
-	
+
 	return recipes, nil
 }
 
@@ -96,12 +96,12 @@ func (s *RecipeService) Update(ctx context.Context, req *domain.UpdateRecipeRequ
 	if err := s.validateUpdateRecipe(*req); err != nil {
 		return nil, fmt.Errorf("cannot update recipe: %v", err)
 	}
-	
+
 	update, err := s.Update(ctx, req)
 	if err != nil {
 		return nil, fmt.Errorf("[recipe service] - unable to update recipe: %v", err)
 	}
-	
+
 	return update, nil
 }
 
@@ -109,10 +109,10 @@ func (s *RecipeService) Delete(ctx context.Context, recipeID uuid.UUID) error {
 	if err := uuid.Validate(recipeID.String()); err != nil {
 		return fmt.Errorf("recipe ID is invalid: %v", err)
 	}
-	
+
 	if err := s.Delete(ctx, recipeID); err != nil {
 		return fmt.Errorf("error deleting recipe: %v", err)
 	}
-	
+
 	return nil
 }
