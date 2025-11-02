89805baca0fd42733fab9e22fecc031c0ba50682
andjrue <drewsimcox12@gmail.com>
2025-11-01 19:38:27 -0600

    feat: recipe service ready to go

--- internal/service/recipe_service.go
+++ internal/service/recipe_service.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"recipe-website/internal/domain"
 	"recipe-website/internal/repository"
+
+	"github.com/google/uuid"
 )
 
 type RecipeService struct {
@@ -19,6 +21,11 @@ func NewRecipeService(rr *repository.PostgresRecipeRepository) *RecipeService {
 }
 
 func (s *RecipeService) validateRecipe(params domain.CreateRecipeRequest) error {
+
+	if err := uuid.Validate(params.UserID.String()); err != nil {
+		return fmt.Errorf("user id invalid: %v", err)
+	}
+
 	if params.Name == "" {
 		return fmt.Errorf("recipe name is required")
 	}
@@ -27,8 +34,8 @@ func (s *RecipeService) validateRecipe(params domain.CreateRecipeRequest) error 
 		return fmt.Errorf("recipe name cannot be longer than 100 characters")
 	}
 
-	if params.UserID == "" {
-		return fmt.Errorf("user id is required to create a recipe")
+	if *params.TimeToCook <= 0 {
+		return fmt.Errorf("time to cook must be greater than 0")
 	}
 
 	return nil
@@ -39,8 +46,8 @@ func (s *RecipeService) validateUpdateRecipe(params domain.UpdateRecipeRequest) 
 		return fmt.Errorf("recipe name cannot be longer than 100 characters")
 	}
 
-	if params.RecipeID == "" {
-		return fmt.Errorf("recipe id is required for updates")
+	if err := uuid.Validate(params.RecipeID.String()); err != nil {
+		return fmt.Errorf("uuid invalid: %v", err)
 	}
 
 	return nil
@@ -53,21 +60,59 @@ func (s *RecipeService) Create(ctx context.Context, req domain.CreateRecipeReque
 
 	recipe, err := s.Create(ctx, req)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create recipe: %w", err)
+		return nil, fmt.Errorf("[recipe service] - failed to create recipe: %w", err)
 	}
 
 	return recipe, nil
 }
 
-func (s *RecipeService) GetByID(ctx context.Context, recipeID string) (*domain.Recipe, error) {
-	if recipeID == "" {
-		return nil, fmt.Errorf("recipe id is required, GetByID")
+func (s *RecipeService) GetByPK(ctx context.Context, recipeID uuid.UUID) (*domain.Recipe, error) {
+	if err := uuid.Validate(recipeID.String()); err != nil {
+		return nil, fmt.Errorf("recipe id malformed, %v", err)
 	}
 
-	recipe, err := s.GetByID(ctx, recipeID)
+	recipe, err := s.GetByPK(ctx, recipeID)
 	if err != nil {
-		return nil, fmt.Errorf("failed to fetch recipe: %w", err)
+		return nil, fmt.Errorf("[recipe service] - failed to fetch recipe: %w", err)
 	}
 
 	return recipe, nil
+}
+
+func (s *RecipeService) GetAllByUserID(ctx context.Context, userID uuid.UUID) ([]domain.Recipe, error) {
+	if err := uuid.Validate(userID.String()); err != nil {
+		return nil, fmt.Errorf("recipe id is required, GetByID")
+	}
+	
+	recipes, err := s.GetAllByUserID(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("[recipe service] - unable to fetch all for user: %v", err)
+	}
+	
+	return recipes, nil
+}
+
+func (s *RecipeService) Update(ctx context.Context, req *domain.UpdateRecipeRequest) (*domain.Recipe, error) {
+	if err := s.validateUpdateRecipe(*req); err != nil {
+		return nil, fmt.Errorf("cannot update recipe: %v", err)
+	}
+	
+	update, err := s.Update(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("[recipe service] - unable to update recipe: %v", err)
+	}
+	
+	return update, nil
+}
+
+func (s *RecipeService) Delete(ctx context.Context, recipeID uuid.UUID) error {
+	if err := uuid.Validate(recipeID.String()); err != nil {
+		return fmt.Errorf("recipe ID is invalid: %v", err)
+	}
+	
+	if err := s.Delete(ctx, recipeID); err != nil {
+		return fmt.Errorf("error deleting recipe: %v", err)
+	}
+	
+	return nil
 }
