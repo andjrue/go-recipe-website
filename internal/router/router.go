ec975574300fe85c7c86a9aab09755cda09750ce
andjrue <drewsimcox12@gmail.com>
2025-11-09 00:50:40 -0700

    add recipe router

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,4 +1,26 @@
 // Package router stores api routes
 package router
 
-// TODO
+import (
+	"recipe-website/internal/handler"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func SetupRoutes(h *handler.Handlers) *chi.Mux{
+	r := chi.NewRouter()
+	
+	r.Use(middleware.Logger)
+	r.Use(middleware.CleanPath)
+	r.Use(middleware.Recoverer)
+	
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Route("/recipes", func(r chi.Router) {
+			r.Post("/", h.Recipe.Create)
+			r.Get("/{id}", h.Recipe.GetByPK)
+		})
+	})
+	
+	return r
+}
