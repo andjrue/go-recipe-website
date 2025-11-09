57e90664ff2f8d12e791fe19d0839a3379397a80
andjrue <drewsimcox12@gmail.com>
2025-11-09 00:50:57 -0700

    add recipe handler

--- internal/handler/recipe_handler.go
+++ internal/handler/recipe_handler.go
@@ -4,6 +4,7 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"recipe-website/internal/domain"
 	"recipe-website/internal/service"
 
 	"github.com/go-chi/chi/v5"
@@ -31,7 +32,7 @@ func NewRecipeHandler(recipeService *service.RecipeService) *RecipeHandler {
 	}
 }
 
-// /recipes/{id}
+// GetByPK - GET /recipes/{id}
 func (h *RecipeHandler) GetByPK(w http.ResponseWriter, r *http.Request) {
 	id, err := uuid.Parse(chi.URLParam(r, "id"))
 	if err != nil {
@@ -48,12 +49,36 @@ func (h *RecipeHandler) GetByPK(w http.ResponseWriter, r *http.Request) {
 	respondJSON(w, http.StatusOK, recipe)
 }
 
+// Create - POST /recipes
+func (h *RecipeHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var req domain.CreateRecipeRequest
+	
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		respondError(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	
+	if req.Name == "" || req.UserID == uuid.Nil {
+		respondError(w, http.StatusBadRequest, "missing required fields: name and user_id")
+		return
+	}
+	
+	recipe, err := h.RecipeService.Create(r.Context(), &req)
+	if err != nil {
+		respondError(w, http.StatusInternalServerError, "failed to create recipe")
+		return
+	}
+	
+	respondJSON(w, http.StatusCreated, recipe)
+}
+
 func respondJSON(w http.ResponseWriter, status int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(data)
 }
 
+// TODO: Set up custom error handling for this. Is and As can get us a long way. 
 func respondError(w http.ResponseWriter, status int, message string) {
 	respondJSON(w, status, map[string]string{"error": message})
 }
