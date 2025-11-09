7bc4b7f3b347a21a678a30bd6fa19232507e77f4
andjrue <drewsimcox12@gmail.com>
2025-11-09 16:29:21 -0700

    add user service

--- internal/service/services.go
+++ internal/service/services.go
@@ -4,10 +4,12 @@ import "recipe-website/internal/repository"
 
 type Services struct {
 	Recipe *RecipeService
+	User *UserService
 }
 
 func SetServices(repos *repository.Repositories) *Services {
 	return &Services{
 		Recipe: NewRecipeService(repos.Recipe),
+		User: NewUserService(repos.User),
 	}
 }
